c880a064e444d582fd9944f6bfd30a637b840daf
Ксения Ерёмина <what.about.ksenia@gmail.com>
2025-11-27 18:28:54 +0300

    Add json and file information

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 text_for_image = input("Введите текст для картинки: ").strip()
 yandex_token = input("Введите яндекс-токен: ").strip()
@@ -20,3 +21,18 @@ params = {
 
 response = requests.post('https://cloud-api.yandex.net/v1/disk/resources/upload',
                          headers=headers, params=params)
+
+# информация о картинке
+params = {
+        'path': f'{group_name}/{filename}.jpg',
+        'fields': 'name,path,size,created,modified,' 
+    }
+    
+response = requests.get('https://cloud-api.yandex.net/v1/disk/resources',
+                           headers=headers, params=params)
+
+# запись в json
+json_filename = f"{filename}_info.json"
+file_info = response.json()
+with open(json_filename, 'w', encoding='utf-8') as json_file:
+    json.dump(file_info, json_file, ensure_ascii=False, indent=2)
